57e43c0407d2a3a187f95e0b95b676ab91254702
Whitedoggy <82828857+Kimgooner@users.noreply.github.com>
2023-05-12 19:26:14 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int X, Y;
+    std::cin >> X >> Y;
+    if(X > 0) {
+        if(Y > 0)
+            std::cout << 1;
+        else
+            std::cout << 4;
+    }
+    else {
+        if(Y > 0)
+            std::cout << 2;
+        else
+            std::cout << 3;
+    }
+}
