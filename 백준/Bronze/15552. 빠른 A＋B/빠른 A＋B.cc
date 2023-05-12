69cbdf3fd25ca3dd4e7c405a859dee0fbddaaba6
Whitedoggy <82828857+Kimgooner@users.noreply.github.com>
2023-05-12 19:26:20 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
+    int T, A, B;
+    std::cin >> T;
+    for(int i = 0; i < T; i++){
+        std::cin >> A >> B;
+        std::cout << A+B << "\n";
+    }
+}
