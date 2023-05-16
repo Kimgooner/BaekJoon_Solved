624dbf98032a623be50e7d01532213b83cab5e8e
Whitedoggy <82828857+Kimgooner@users.noreply.github.com>
2023-05-16 23:35:45 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 120 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool cmp(std::pair<int,int> p1, std::pair<int,int> p2){
+    if(p1.second < p2.second) return true;
+    else if(p1.second == p2.second){
+        if(p1.first < p2.first) return true;
+        else return false;
+    }
+    else return false;
+}
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    int n1, n2;
+    std::vector<std::pair<int,int>> nums;
+    for(int i = 0; i < N; i++){
+        std::cin >> n1 >> n2;
+        nums.emplace_back(n1, n2);
+    }
+    std::sort(nums.begin(), nums.end(), cmp);
+
+    for(std::pair<int,int> p : nums)
+        std::cout << p.first << " " << p.second << "\n";
+}
