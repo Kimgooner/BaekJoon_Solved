d19d088a14b670e74e9cb139e4527e79be613fa7
Whitedoggy <82828857+Kimgooner@users.noreply.github.com>
2023-05-30 16:00:47 +0900

    [Silver III] Title: N과 M (3), Time: 488 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int N, M;
+int nums[9] = {0, };
+bool check[9] = {0, };
+
+void find(int n){
+    if(n == M) {
+        for (int i = 0; i < M; i++) {
+            std::cout << nums[i] << " ";
+        }
+        std::cout << "\n";
+    }
+    else {
+        for(int i = 1; i <= N; i++){
+            check[i] = true;
+            nums[n] = i;
+            find(n+1);
+            check[i] = false;
+        }
+    }
+}
+
+int main() {
+    std::cin >> N >> M;
+    find(0);
+}
