5f87dc51b4bc47ed3419f8537dfc84f32e2066f2
Whitedoggy <82828857+Kimgooner@users.noreply.github.com>
2025-05-30 11:33:16 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i = 1; i <= 1000; i++){
+        if(i*i == n) return 1;
+    }
+    return 2;
+}
