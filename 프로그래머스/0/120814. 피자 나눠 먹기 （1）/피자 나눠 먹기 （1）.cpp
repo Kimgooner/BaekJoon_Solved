4479def8f229107a391f802158bbed50052b407d
Whitedoggy <82828857+Kimgooner@users.noreply.github.com>
2025-05-30 11:34:22 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    while(7*answer / n < 1){
+        answer++;
+        
+    }
+    
+    return answer;
+}
